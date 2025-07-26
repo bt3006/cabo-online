24ce9359a722bdcecd01f6deefd9d16f5b209993
bastian tisch <tischb@yahoo.de>
2025-07-26 23:27:01 +0200

    Spielernamen hinzugefügt

--- cabo_web.py
+++ cabo_web.py
@@ -1,6 +1,15 @@
 import streamlit as st
 
-st.set_page_config(page_title="Cabo Online", page_icon="🎴")
+st.set_page_config(page_title="Cabo – Web Edition", page_icon="🧠")
 
-st.title("🎴 Cabo Online")
-st.write("Willkommen zu deiner gehosteten Version!")
+# Titel der App
+st.title("🃏 Cabo – Web Edition")
+
+# Sidebar für Spielereingabe
+st.sidebar.header("Spielereinstellungen")
+spieler1_name = st.sidebar.text_input("👤 Name Spieler 1", "Spieler 1")
+spieler2_name = st.sidebar.text_input("👤 Name Spieler 2", "Spieler 2")
+
+# Im Session-State speichern (optional, falls später weiterverwendet)
+st.session_state.spieler1_name = spieler1_name
+st.session_state.spieler2_name = spieler2_name
